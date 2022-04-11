44928c02fd9b34058cc4420dc3e7c52303bbbd57
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-04-11 10:48:49 +0400

    [Build] Enable context receivers feature in :compiler:fir:fir2rir

--- build.gradle.kts
+++ build.gradle.kts
@@ -365,6 +365,10 @@ val projectsWithDisabledFirBootstrap = coreLibProjects + listOf(
     ":wasm:wasm.ir"
 )
 
+val projectsWithEnabledContextReceivers = listOf(
+    ":compiler:fir:fir2ir"
+)
+
 val gradlePluginProjects = listOf(
     ":kotlin-gradle-plugin",
     ":kotlin-gradle-plugin-api",
@@ -519,7 +523,8 @@ allprojects {
         kotlinOptions {
             freeCompilerArgs = commonCompilerArgs + jvmCompilerArgs
 
-            if (useJvmFir && this@allprojects.path !in projectsWithDisabledFirBootstrap) {
+            val moduleName = this@allprojects.path
+            if (useJvmFir && moduleName !in projectsWithDisabledFirBootstrap) {
                 freeCompilerArgs += "-Xuse-fir"
                 freeCompilerArgs += "-Xabi-stability=stable"
                 if (useFirLT) {
@@ -531,6 +536,11 @@ allprojects {
             }
             if (renderDiagnosticNames) {
                 freeCompilerArgs += "-Xrender-internal-diagnostic-names"
+            }
+
+            if (moduleName in projectsWithEnabledContextReceivers) {
+                freeCompilerArgs += "-Xcontext-receivers"
+                freeCompilerArgs += "-Xdont-poison-binaries-with-prerelease"
             }
         }
     }
