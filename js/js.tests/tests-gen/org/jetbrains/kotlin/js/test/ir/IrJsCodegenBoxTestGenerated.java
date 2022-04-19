2ace9b49ee9dcb89408936e7c48fe32ca7f128d0
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2022-04-20 00:07:17 +0300

    [FIR2IR] Consider property init value from nested class, ^KT-52057 Fixed

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
@@ -19824,6 +19824,12 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
         }
 
         @Test
+        @TestMetadata("considerPropertyInitValueFromNestedClass.kt")
+        public void testConsiderPropertyInitValueFromNestedClass() throws Exception {
+            runTest("compiler/testData/codegen/box/innerNested/considerPropertyInitValueFromNestedClass.kt");
+        }
+
+        @Test
         @TestMetadata("createNestedClass.kt")
         public void testCreateNestedClass() throws Exception {
             runTest("compiler/testData/codegen/box/innerNested/createNestedClass.kt");
--- native/native.tests/tests-gen/org/jetbrains/kotlin/konan/blackboxtest/NativeCodegenBoxTestGenerated.java
+++ native/native.tests/tests-gen/org/jetbrains/kotlin/konan/blackboxtest/NativeCodegenBoxTestGenerated.java
@@ -22463,6 +22463,12 @@ public class NativeCodegenBoxTestGenerated extends AbstractNativeCodegenBoxTest 
             }
 
             @Test
+            @TestMetadata("considerPropertyInitValueFromNestedClass.kt")
+            public void testConsiderPropertyInitValueFromNestedClass() throws Exception {
+                runTest("compiler/testData/codegen/box/innerNested/considerPropertyInitValueFromNestedClass.kt");
+            }
+
+            @Test
             @TestMetadata("createNestedClass.kt")
             public void testCreateNestedClass() throws Exception {
                 runTest("compiler/testData/codegen/box/innerNested/createNestedClass.kt");
